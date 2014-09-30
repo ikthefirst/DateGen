45fd03147a5de89be5c72f7fff2911c013b780f4
ikthefirst <ikthefirst@gmail.com>
2014-09-30 15:47:47 +0200

    Initial project creation with maven artifact.

---
+++ DateGen/src/main/java/code/ikthefirst/dategen/App.java
@@ -0,0 +1,13 @@
+package code.ikthefirst.dategen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
